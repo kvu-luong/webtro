36bd47f5fb65fac30991578e2eb33ac3e6bad186
ntt-vulk <vu.lk@nttnetworks.com>
2020-12-16 17:20:55 +0700

    inital setup

---
+++ webtro/src/home.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="homepage">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home</title>
+        <link rel="stylesheet" href="../assets/css/style.css">
+        <link rel="author" href="humans.txt">
+    </head>
+    <body>
+        <header>
+        	<section id="nav">
+        		<div class="nav-left">
+        			<div>Menu</div>
+        			<div>Search</div>
+        			<div>Language</div>
+        		</div>
+        		<div class="nav-center">
+        			<div class="logo"><a href='#'>Logo</a></div>
+        		</div>
+        		<div class="nav-right">
+        			<div>Switch theme color</div>
+        			<div>Ring</div>
+        			<div>Account</div>
+        		</div>
+        	</section>
+        	<section id="sidebar">
+        		<div class="main">
+        		</div>
+        		<div class="sub">
+        		</div>
+        	</section>
+        </header>
+
+        <section id="js-theme-color">
+        </section>
+
+        <main>
+        	<div class="container-fluid">
+        		<div class="dashboard-wrapper">
+        		</div>
+        	</div>
+        </main>
+
+        <footer>
+        	<div class="container-fluid">
+        		
+        	</div>
+        </footer>
+        <script src="../assets/js/script.js"></script>
+    </body>
+</html>
